26a5f493563a8e337df31ee84dff0dfea4236431
Jakub Košvanec <xkosvan@fi.muni.cz>
2022-05-30 15:27:14 +0200

    fix: Code cleanup

--- frontend/src/components/Application/ApplicationPage.tsx
+++ frontend/src/components/Application/ApplicationPage.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Header from "./Header/Header";
 import Index from "./Index/IndexPage";
 import OffersPage from "./Offers/OffersPage";
@@ -9,12 +9,12 @@ export const ApplicationPage = () => {
 
   return (
     <div>
-      <Header />
+      <Header/>
       <Routes>
-        <Route path="offers/*" element={<OffersPage />}/>
-        <Route path='user/:name' element={<UserPage />} />
-        <Route path='offer/:id' element={<OfferPage />} />
-        <Route path='/' element={<Index/>} />
+        <Route path="offers/*" element={<OffersPage/>}/>
+        <Route path='user/:name' element={<UserPage/>}/>
+        <Route path='offer/:id' element={<OfferPage/>}/>
+        <Route path='/' element={<Index/>}/>
       </Routes>
     </div>
   );
