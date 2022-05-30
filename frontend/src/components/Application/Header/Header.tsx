b93ec25067772a5729a38c4524c0dd6a81c855c0
Jakub Košvanec <xkosvan@fi.muni.cz>
2022-05-30 12:31:25 +0200

    feat: Header change

--- frontend/src/components/Application/Header/Header.tsx
+++ frontend/src/components/Application/Header/Header.tsx
@@ -4,13 +4,13 @@ import Logo from "./Logo";
 import Searchbar from "./Searchbar";
 
 export const Header = () => {
-  
+
   return (
-    <header className="bg-orange-400 h-30 flex justify-between items-center">
+    <header className="bg-space-cadet h-20 flex justify-between items-center">
       <Logo />
       <Searchbar />
       <div>
-        <ul className="flex">
+        <ul className="flex text-mint-cream">
           <HeaderItem text="Login" linkTo="/login" />
           <HeaderItem text="Register" linkTo="/register" />
         </ul>
--- frontend/src/components/Application/Header/Logo.tsx
+++ frontend/src/components/Application/Header/Logo.tsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import logo from '../../../../public/assets/new-logo.png';
 
 
 export const Logo = () => {
   return (
     <Link to="/">
-      <img className="h-16" alt="Server's logo" src="/assets/logo.png"/>
+      <img className="h-16 mb-2 ml-3 mr-4" alt="Server's logo" src={logo}/>
     </Link>
   );
 };
