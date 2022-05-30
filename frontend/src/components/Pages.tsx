fa61c5f59a0c3a8fd39fda4bace2270641ffb598
Jakub Košvanec <xkosvan@fi.muni.cz>
2022-05-30 15:27:33 +0200

    fix: Code cleanup

--- frontend/src/components/Pages.tsx
+++ frontend/src/components/Pages.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import ApplicationPage from "./Application/ApplicationPage";
 import LoginPage from "./Login/LoginPage";
 import RegisterPage from "./Register/RegisterPage";
@@ -9,9 +9,9 @@ export const Pages = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage />}/>
-        <Route path="/register" element={<RegisterPage />}/>
-        <Route path='/*' element={<ApplicationPage />} />
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/register" element={<RegisterPage/>}/>
+        <Route path='/*' element={<ApplicationPage/>}/>
       </Routes>
     </BrowserRouter>
   );
