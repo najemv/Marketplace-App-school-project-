8b110b0bc08b9b2ef743be2e28b6b29cf1b604dd
Jakub Košvanec <xkosvan@fi.muni.cz>
2022-05-30 14:07:42 +0200

    feat: Change Navigation to List

--- frontend/src/components/Application/Offers/Navigation/Navigation.tsx
+++ frontend/src/components/Application/Offers/Navigation/Navigation.tsx
@@ -1,24 +1,26 @@
 import useSWR from "swr";
-import { Category, CategoryPreview } from "../../../../types";
+import {CategoryPreview} from "../../../../types";
 import fetcher from "../../../../utils/fetcher";
 import CategoryCard from "./CategoryCard";
 
 export const Navigation = () => {
 
-  const { data, error } = useSWR(`http://localhost:4000/category`, fetcher);
+  const {data, error} = useSWR(`http://localhost:4000/category`, fetcher);
 
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
   const categories: CategoryPreview[] = data.data;
 
   return (
-    <nav className="m-10 bg-space-cadet p-5 text-center rounded-2xl text-2xl">
-      <h1 className="text-mint-cream font-bold">Categories</h1>
-      <ul>
-        <CategoryCard name="All" id={0} />
-        {categories.map((cat) => <CategoryCard key={cat.id} {...cat}/>)}
-      </ul>
-    </nav>
+    <div>
+      <nav className="sticky top-5 z-40 m-10 bg-space-cadet p-5 text-center rounded-2xl text-2xl">
+        <h1 className="text-mint-cream font-bold">Categories</h1>
+        <ul role="list">
+          <CategoryCard name="All" id={0}/>
+          {categories.map((cat) => <CategoryCard key={cat.id} {...cat}/>)}
+        </ul>
+      </nav>
+    </div>
   );
 };
 
