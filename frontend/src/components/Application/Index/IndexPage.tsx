412682c33701444dac8f4cad5067491a784b2488
Jakub Košvanec <xkosvan@fi.muni.cz>
2022-05-30 15:26:33 +0200

    fix: Code cleanup

--- frontend/src/components/Application/Index/IndexPage.tsx
+++ frontend/src/components/Application/Index/IndexPage.tsx
@@ -1,12 +1,10 @@
-import { Link } from "react-router-dom";
-import ListOfOffers from "../Offers/ListOfOffers";
 import Navigation from "../Offers/Navigation/Navigation";
 
 export const IndexPage = () => {
 
   return (
     <div className="flex">
-      <Navigation />
+      <Navigation/>
     </div>
   );
 };
