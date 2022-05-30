d9d9de5de1e40e6a896f587c1c1e19cfbc9a25a5
Jakub Košvanec <xkosvan@fi.muni.cz>
2022-05-30 15:26:43 +0200

    fix: Code cleanup

--- frontend/src/components/Application/Offers/Navigation/CategoryCard.tsx
+++ frontend/src/components/Application/Offers/Navigation/CategoryCard.tsx
@@ -1,17 +1,18 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 interface CategoryCardProps {
   id: number;
   name: string;
-};
+}
 
 export const CategoryCard = ({id, name}: CategoryCardProps) => {
   const goToLink = id != 0 ? `${id}-${name}` : "";
   return (
     <Link to={`/offers/${goToLink}`}>
-    <li className="m-2 bg-imperial-red hover:bg-medium-candy-apple-red rounded-2xl text-center font-medium px-5 py-2 text-base">
+      <li
+        className="m-2 bg-imperial-red hover:bg-medium-candy-apple-red rounded-2xl text-center font-medium px-5 py-2 text-base">
         {name.toUpperCase()}
-    </li>
+      </li>
     </Link>
   );
 };
